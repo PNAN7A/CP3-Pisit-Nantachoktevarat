0b5d768b3c2aeff7d72f86604a30551707aab317
Pisit Nantachoktevarat <ta.pisit@icloud.com>
2020-04-08 23:41:09 +0700

    Exercise 4
    Pisit Nantachoktevarat

---
+++ Exercise4_Pisit_N.py
@@ -0,0 +1,9 @@
+a = 100
+b = 95.17
+c = 77
+d = 99
+print("--- Your Score ---")
+print("Foundation English :", a)
+print("General Business :", b)
+print("Introduction to Computer Systems :", c)
+print("Computer Programming :", d)
