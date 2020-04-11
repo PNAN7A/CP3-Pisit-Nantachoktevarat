145a8e3d31ed02369c9de8166333999b717742df
Pisit Nantachoktevarat <ta.pisit@icloud.com>
2020-04-11 22:44:30 +0700

    Ex5_2 Added ?

--- Exercise5_2_Pisit_N.py
+++ Exercise5_2_Pisit_N.py
@@ -1,6 +1,6 @@
 print("Calculator The Driving Speed")
 print("----------")
-input("Where do you want to travel : ")
+input("Where do you want to travel? : ")
 length = float(input("The distance (km) = "))
 time = float(input("Time of arrival (hr) = "))
 print("----------")
