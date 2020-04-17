5fe85cb92684c569a996ec96515b117b95f64c37
Pisit Nantachoktevarat <ta.pisit@icloud.com>
2020-04-17 22:01:32 +0700

    Lecture50

---
+++ Lecture50_Pisit_N.py
@@ -0,0 +1,12 @@
+def addNum(x,y):
+    print(x+y)
+addNum(100,25)
+def subNum(x,y):
+    print(x-y)
+subNum(100,25)
+def mulNum(x,y):
+    print(x*y)
+mulNum(5,5)
+def divNum(x,y):
+    print(x/y)
+divNum(100,25)
